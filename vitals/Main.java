1c2b1e685707bceaecdf9bfaf50638821289811d
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-08-28 09:49:45 +0530

    Update Main.java
    added some subfunctions and its testcases

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,23 +1,33 @@
 package vitals;
 
-public class Main {
+public class BatteryChecker {
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+        return isTemperatureInRange(temperature) &&
+               isStateOfChargeInRange(soc) &&
+               isChargeRateInRange(chargeRate);
+    }
+
+    private static boolean isTemperatureInRange(float temperature) {
+        return temperature >= 0 && temperature <= 45;
+    }
+
+    private static boolean isStateOfChargeInRange(float soc) {
+        return soc >= 20 && soc <= 80;
+    }
+
+    private static boolean isChargeRateInRange(float chargeRate) {
+        return chargeRate <= 0.8;
     }
 
     public static void main(String[] args) {
         assert(batteryIsOk(25, 70, 0.7f) == true);
         assert(batteryIsOk(50, 85, 0.0f) == false);
-        System.out.println("Some more tests needed");
+        assert(isTemperatureInRange(-4) == false); 
+        assert(isStateOfChargeInRange(5) == false); 
+        assert(isChargeRateInRange(1.1f) == false);
+        assert(isTemperatureInRange(44) == true);
+        assert(isStateOfChargeInRange(21) == true); 
+        assert(isChargeRateInRange(0.1f) == true);
     }
 }
+
