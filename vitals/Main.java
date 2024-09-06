5b3933f8b82ca61c44bbc4018494183f1d04d2ae
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-09-06 14:12:19 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -4,9 +4,9 @@ public class Main {
       public static void main(String[] args) {
             assert (BatteryChecker.batteryIsOk(0, 20, 0.8f)); // All  parameters at maximum value
             assert (BatteryChecker.batteryIsOk(10, 25, 0.5f));  // All parameters in range
-            assert (!BatteryChecker.batteryIsOk(50, 85, 0.0f)); // Everything out of range
-            assert (!BatteryChecker.batteryIsOk(-10, 70, 0.5f)); //Temperature Out of range
-            assert (!BatteryChecker.batteryIsOk(25, 90, 0.5f)); // State Of Charge Out of range
-            assert (!BatteryChecker.batteryIsOk(25, 70, 1.0f)); // Charge Rate Out of range
+            assert (!BatteryChecker.batteryIsOk(100, 100, 0.0f)); // Everything out of range
+            assert (!BatteryChecker.batteryIsOk(-25, 50, 0.4f)); //Temperature Out of range
+            assert (!BatteryChecker.batteryIsOk(30, 100, 0.4f)); // State Of Charge Out of range
+            assert (!BatteryChecker.batteryIsOk(10, 55, 10.0f)); // Charge Rate Out of range
     }
 }
