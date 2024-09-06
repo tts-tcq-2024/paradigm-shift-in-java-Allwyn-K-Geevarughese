a91f2e2da267d9a522a43445c7179647b968be77
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-09-06 10:58:59 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,32 +1,14 @@
 package vitals;
 
 public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        return isTemperatureOk(temperature) && isStateOfChargeOk(soc) && isChargeRateOk(chargeRate);
-    }
-
-    private static boolean isTemperatureOk(float temperature) {
-        return temperature >= 0 && temperature <= 45;
-    }
-
-    private static boolean isStateOfChargeOk(float soc) {
-        return soc >= 20 && soc <= 80;
-    }
-
-    private static boolean isChargeRateOk(float chargeRate) {
-        return chargeRate <= 0.8;
-    }
-
-    public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        assert(isTemperatureOk(-4) == false); 
-        assert(isStateOfChargeOk(5) == false); 
-        assert(isChargeRateOk(1.1f) == false);
-        assert(isTemperatureOk(44) == true);
-        assert(isStateOfChargeOk(21) == true); 
-        assert(isChargeRateOk(0.1f) == true);
-        System.out.println("All tests passed");
+      public static void main(String[] args) {
+          
+        assert (BatteryChecker.batteryIsOk(25, 70, 0.7f));
+        assert (!BatteryChecker.batteryIsOk(50, 85, 0.0f));
+        assert (!BatteryChecker.batteryIsOk(-10, 70, 0.5f));
+        assert (!BatteryChecker.batteryIsOk(25, 90, 0.5f));
+        assert (!BatteryChecker.batteryIsOk(25, 70, 1.0f));
+        assert (BatteryChecker.batteryIsOk(0, 20, 0.8f));
     }
 }
 
