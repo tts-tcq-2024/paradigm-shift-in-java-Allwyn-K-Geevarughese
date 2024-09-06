479cf3d36108e9df52a5bfff45b218dbb02126e0
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-09-06 12:17:12 +0530

    Update BatteryChecker.java

--- vitals/BatteryChecker.java
+++ vitals/BatteryChecker.java
@@ -3,15 +3,15 @@ package vitals;
 public class BatteryChecker {
 
     public static boolean checkTemperatureInRange(float temperature) {
-        return RangeValidator.isParameterInrange(temperature, 0, 45, "Temperature out of range!");
+        return RangeValidator.isParameterInRange(temperature, 0, 45, "Temperature out of range!");
     }
 
     public static boolean checkSocInRange(float soc) {
-        return RangeValidator.isParameterInrange(soc, 20, 80, "State of Charge out of range!");
+        return RangeValidator.isParameterInRange(soc, 20, 80, "State of Charge out of range!");
     }
 
     public static boolean checkChargeRateInRange(float chargeRate) {
-        return RangeValidator.isParameterInrange(chargeRate, 0, 0.8f, "Charge Rate out of range!");
+        return RangeValidator.isParameterInRange(chargeRate, 0, 0.8f, "Charge Rate out of range!");
     }
 
     public static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
