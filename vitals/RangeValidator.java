e3e921b360bcb4c39c7691197bca4266714285bd
Allwyn K Geevarughese <66422903+Allwyn-K-Geevarughese@users.noreply.github.com>
2024-09-13 16:09:41 +0530

    Update RangeValidator.java

--- vitals/RangeValidator.java
+++ vitals/RangeValidator.java
@@ -2,6 +2,7 @@ package vitals;
 
 public class RangeValidator {
 
+    ////Parameters with minimum value and maximum value
     public static boolean isParameterInRange(float actualValue, float minValue, float maxValue, String message) {
         if (maxValue < actualValue || minValue > actualValue) {
             System.out.println(message);
@@ -9,6 +10,8 @@ public class RangeValidator {
         }
         return true;
     }
+
+    //Parameters with no minimum value
     public static boolean isParameterInRange(float actualValue, float maxValue, String message) {
         return isParameterInRange(actualValue, Float.MIN_VALUE, maxValue, message);
     }
